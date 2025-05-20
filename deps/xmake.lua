9ca622fe0f08d159ee3e777883133ba991c20066
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2025-05-20 13:04:55 +0200

    add sndfilter package xmake.lua

---
+++ deps/xmake.lua
@@ -0,0 +1,18 @@
+package("sndfilter")
+    set_homepage("https://github.com/velipso/sndfilter")
+    set_description("Algorithms for sound filters, like reverb, dynamic range compression, lowpass, highpass, notch, etc")
+    set_license("0-bsd")
+
+    add_urls("https://github.com/velipso/sndfilter.git")
+
+    add_includedirs("src")
+
+    on_install(function(package)
+        os.cd("sndfilter")
+        os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        import("package.tools.xmake").install(package)
+    end)
+
+    on_test(function(package)
+        assert(package:has_cfuncs("sf_snd_new", {includes = "snd.h"}
+    end)
